5e10779da10c5471404f4a117a952b9a6d04d916
Kevin McDonnell <kevin.mcdonnell@cps.co.uk>
2020-10-17 22:41:36 +0100

    even simpler

--- scripts/runmerge.py
+++ scripts/runmerge.py
@@ -1,4 +1,4 @@
 import os
 import MarkdownTools2
 
-mdmerge -o docs/merged.md ${PWD}/docs/By-approach/asking-questionsorig.md ${PWD}/docs/By-approach/communicatingorig.md ${PWD}/docs/By-approach/files.md ${PWD}/docs/By-approach/liking-posts ${PWD}/docs/By-approach/voice.md ${PWD}/docs/By-approach/working-out-loud.md ${PWD}/docs/By-service/onenoteorig.md ${PWD}/docs/By-service/outlookorig.md ${PWD}/docs/By-service/plannerorig.md ${PWD}/docs/By-service/teamsorig.md
+mdmerge -o docs/merged.md ${PWD}/docs/By-approach/asking-questionsorig.md ${PWD}/docs/By-approach/communicatingorig.md
