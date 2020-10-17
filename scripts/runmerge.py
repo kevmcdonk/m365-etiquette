f2e0306aea5180f52f80d054fe385238199cb425
Kevin McDonnell <kevin.mcdonnell@cps.co.uk>
2020-10-17 23:17:45 +0100

    Function led one

--- scripts/runmerge.py
+++ scripts/runmerge.py
@@ -1,11 +1,15 @@
 import os
 
+def runmerge(folderPath)
+    dirFiles = os.listdir('./' + folderPath)
+    fileList = ""
+    for foundFile in dirFiles:
+        fileList += "\"${PWD}/" + folderPath + "/" + foundFile + "\" "
+    print(fileList)
+
+    os.system('mdmerge -o docs/By-approach/Asking-questions.md ' + fileList)
+    return
+
 os.system('mdmerge -o docs/merged.md ${PWD}/docs/By-approach/asking-questionsorig.md ${PWD}/docs/By-approach/communicatingorig.md ${PWD}/docs/By-approach/files.md ${PWD}/docs/By-approach/liking-posts ${PWD}/docs/By-approach/voice.md ${PWD}/docs/By-approach/working-out-loud.md ${PWD}/docs/By-service/onenoteorig.md ${PWD}/docs/By-service/outlookorig.md ${PWD}/docs/By-service/plannerorig.md ${PWD}/docs/By-service/teamsorig.md')
 
-dirFiles = os.listdir('./content/By approach/Asking questions')
-fileList = ""
-for foundFile in dirFiles:
-    fileList += "\"${PWD}/content/By approach/Asking questions/" + foundFile + "\" "
-print(fileList)
-
-os.system('mdmerge -o docs/By-approach/Asking-questions.md ' + fileList)
+funMerge('content/By approach/Asking questions')
