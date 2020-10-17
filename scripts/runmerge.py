2a7dfdfd1c98a1a20485f0330c9548645ade947c
Kevin McDonnell <kevin.mcdonnell@cps.co.uk>
2020-10-17 23:24:05 +0100

    Neatly deployed with a function

--- scripts/runmerge.py
+++ scripts/runmerge.py
@@ -13,3 +13,8 @@ def runMerge(folderPath):
 os.system('mdmerge -o docs/merged.md ${PWD}/docs/By-approach/asking-questionsorig.md ${PWD}/docs/By-approach/communicatingorig.md ${PWD}/docs/By-approach/files.md ${PWD}/docs/By-approach/liking-posts ${PWD}/docs/By-approach/voice.md ${PWD}/docs/By-approach/working-out-loud.md ${PWD}/docs/By-service/onenoteorig.md ${PWD}/docs/By-service/outlookorig.md ${PWD}/docs/By-service/plannerorig.md ${PWD}/docs/By-service/teamsorig.md')
 
 runMerge('content/By approach/Asking questions')
+runMerge('content/By approach/Communicating')
+runMerge('content/By service/OneNote')
+runMerge('content/By service/Outlook')
+runMerge('content/By service/Planner')
+runMerge('content/By service/Teams')
