9b6ee3eb038e55c26776d731161b412e42a79ae5
Kevin McDonnell <kevin.mcdonnell@cps.co.uk>
2020-10-17 23:28:27 +0100

    Humph

--- scripts/runmerge.py
+++ scripts/runmerge.py
@@ -7,7 +7,8 @@ def runMerge(folderPath):
         fileList += "\"${PWD}/" + folderPath + "/" + foundFile + "\" "
     print(fileList)
 
-    os.system('mdmerge -o docs/By-approach/Asking-questions.md ' + fileList)
+    docsFolder = folderPath.replace('content/','docs/')
+    os.system('mdmerge -o ' + docsFolder + '.md ' + fileList)
     return
 
 os.system('mdmerge -o docs/merged.md ${PWD}/docs/By-approach/asking-questionsorig.md ${PWD}/docs/By-approach/communicatingorig.md ${PWD}/docs/By-approach/files.md ${PWD}/docs/By-approach/liking-posts ${PWD}/docs/By-approach/voice.md ${PWD}/docs/By-approach/working-out-loud.md ${PWD}/docs/By-service/onenoteorig.md ${PWD}/docs/By-service/outlookorig.md ${PWD}/docs/By-service/plannerorig.md ${PWD}/docs/By-service/teamsorig.md')
