259c50695ac35dde16e453a12f2bf0bea8605cf3
Kevin McDonnell <kevin.mcdonnell@cps.co.uk>
2020-10-17 22:57:14 +0100

    Merging

--- scripts/runmerge.py
+++ scripts/runmerge.py
@@ -5,7 +5,8 @@ os.system('mdmerge -o docs/By-service/onenote.md "${PWD}/content/OneNote.md" "${
 os.system('mdmerge -o docs/By-service/outlook.md "${PWD}/content/Outlook.md" "${PWD}/content/Outlook-Email-At mentions.md" "${PWD}/content/Outlook-Email-CC people.md" "${PWD}/content/Outlook-Email-People in to line.md" "${PWD}/content/Outlook-Email-Read receipts.md" "${PWD}/content/Outlook-Email-Signatures.md" "${PWD}/content/Outlook-Meetings-Agenda.md" "${PWD}/content/Outlook-Meetings-Findtime.md" "${PWD}/content/Outlook-Meetings-Scheduling assistant.md" "${PWD}/content/Outlook-OOO-Dates and contacts.md" "${PWD}/content/Outlook-OOO-Do not leave OOO on.md" "${PWD}/content/Outlook-OOO-Expectations.md"')
 os.system('mdmerge -o docs/By-service/planner.md "${PWD}/content/Planner.md" "${PWD}/content/Planner-Assignments.md" "${PWD}/content/Planner-Dates-Filter.md" "${PWD}/content/Planner-Dates-Flagged tasks.md" "${PWD}/content/Planner-Dates-Scheduled.md" "${PWD}/content/Planner-Dates-Set dates.md" "${PWD}/content/Planner-Labels-Categorise.md" "${PWD}/content/Planner-Labels-Filter.md" "${PWD}/content/Planner-Labels-Group tasks by label.md" "${PWD}/content/Planner-Use of buckets.md"')
 
-dirFiles = os.listdir('.')
-dirFiles.sort()
+dirFiles = os.listdir('./content')
+fileList = ""
 for foundFile in dirFiles:
-    print(foundFile)
+    fileList += foundFile + " "
+print(fileList)
