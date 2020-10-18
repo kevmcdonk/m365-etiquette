fb3a045e1d298bfd3f6f1534f129c128d1a825f8
Kevin McDonnell <kevin.mcdonnell@cps.co.uk>
2020-10-18 21:04:14 +0100

    Moving around all over the place

--- docs/By-approach/Files/Files-Accessing.md
+++ docs/By-approach/Files/Files-Accessing.md
@@ -1,18 +1,13 @@
-# Files
-
-## Where should I store files?
-
-Who needs to see it?
-- Only me - OneDrive for Business
-- A few select people before the rest of the team - OneDrive for Business
-- A few external people who do not collaborate in a Microsoft Team - OneDrive for Business
-- My team, or the team I am working with - a Channel within Microsoft Teams
-- The whole organisation - Intranet
-
-Should others be able to edit the file?
-- Yes - OneDrive for Business or a Channel within Microsoft Teams
-- No - Intranet or a library within SharePoint with locked down permissions
-
+---
+title: "Accessing files"
+author: GreyHatBeard
+tags: 
+  - By Approach
+  - Files
+source: Internal Contribution
+audience: End User
+strength: Should
+---
 ##	How can I access files?
 
 My Files in OneDrive for Business
--- scripts/runmerge.py
+++ scripts/runmerge.py
@@ -7,16 +7,19 @@ def runMerge(folderPath):
         fileList += "\"${PWD}/" + folderPath + "/" + foundFile + "\" "
     print(fileList)
 
-    docsFolder = folderPath.replace('content/','docs/')
-    os.system('mdmerge -o ' + docsFolder + '.md ' + fileList)
+    os.system('mdmerge -o ' + folderPath + '.md ' + fileList)
     return
 
 os.system('mdmerge -o docs/merged.md ${PWD}/docs/By-approach/asking-questionsorig.md ${PWD}/docs/By-approach/communicatingorig.md ${PWD}/docs/By-approach/files.md ${PWD}/docs/By-approach/liking-posts ${PWD}/docs/By-approach/voice.md ${PWD}/docs/By-approach/working-out-loud.md ${PWD}/docs/By-service/onenoteorig.md ${PWD}/docs/By-service/outlookorig.md ${PWD}/docs/By-service/plannerorig.md ${PWD}/docs/By-service/teamsorig.md')
 
-runMerge('content/By-approach/Asking-questions')
-runMerge('content/By-approach/Communicating')
-runMerge('content/By-approach/Liking-posts')
-runMerge('content/By-service/OneNote')
-runMerge('content/By-service/Outlook')
-runMerge('content/By-service/Planner')
-runMerge('content/By-service/Teams')
+runMerge('docs/By-approach/Asking-questions')
+runMerge('docs/By-approach/Communicating')
+runMerge('docs/By-approach/Liking-posts')
+runMerge('docs/By-approach/Files')
+runMerge('docs/By-approach/Voice')
+runMerge('docs/By-approach/Working-out-loud')
+
+runMerge('docs/By-service/OneNote')
+runMerge('docs/By-service/Outlook')
+runMerge('docs/By-service/Planner')
+runMerge('docs/By-service/Teams')
